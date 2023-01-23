be3a10a7e2b5b555555792e1091da1f4af9ddd08
Nurlanov-Temirlan <118036633+Nurlanov-Temirlan@users.noreply.github.com>
2023-01-23 21:21:03 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn=document.querySelector('.button')
+
+function hendleBtnClick(event){
+    event.preventDefault();
+    let counteClickBtn=btn.textContent;
+    btn.textContent=parseInt(counteClickBtn)+1;
+
+}
+
+btn.addEventListener('click',hendleBtnClick);
